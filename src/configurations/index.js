4c9364ff40ef3dddc317c230923553b98951552f
Võ Tiến Khoa <votienkhoa16vl@gmail.com>
2023-08-30 13:23:55 +0700

    Add isDevEnv and endPoint value

--- src/configurations/index.js
+++ src/configurations/index.js
@@ -3,6 +3,10 @@ const envConfig = {
     prodEndPoint: 'gateway.dev.meu-solutions.com/meu-shop-pr-be/api/v1.0',
 };
 
+const isDevEnv = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+const endPoint = isDevEnv ? envConfig.devEndPoint : envConfig.prodEndPoint;
+
 export const config = {
     envConfig,
 };
